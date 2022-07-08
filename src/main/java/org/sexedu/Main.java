54945e868e87467f58f1ee58712c278f396a9aa0
ppfirework <3212329718@qq.com>
2022-07-08 22:09:01 +0800

    auto push data to mongodb a

--- src/main/java/org/sexedu/Main.java
+++ src/main/java/org/sexedu/Main.java
@@ -1,37 +1,76 @@
 package org.sexedu;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.jsoup.Jsoup;
 
+import java.math.BigInteger;
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import static org.sexedu.Tools.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    static MongoClient client = new MongoClient("127.0.0.1", 27017);
+    static MongoDatabase database = client.getDatabase("bornhub");
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        List<String> url = exploreURL(480);
-        int thread = 8;
-        ExecutorService executorService = Executors.newFixedThreadPool(thread);
-        for (int i = 0; i < url.size(); i++) {
-            String s = url.get(i);
-            executorService.execute(() -> {
-                Result result = getInfo(Jsoup.parse(getHTML(s)));
-                log("标题" + result.getTitle());
-                log("播放量" + result.getView());
-                log("标题" + result.getTitle());
-                log("视频链接" + result.getVideoLink());
-            });
+        //get all the collections
+        MongoCollection<Document> collection = database.getCollection("videos");
+        BigInteger id = BigInteger.valueOf(getMaxId(collection));
+        List<String> url = Tools.exploreURL(20);
+        ExecutorService executor = Executors.newFixedThreadPool(8);
+        executor.execute(() -> {
+            for (String s : url) {
+                Result result = Tools.getInfo(Jsoup.parse(Tools.getHTML(s)));
+                String title = result.getTitle();
+                String link = result.getVideoLink();
+                String tag = result.getTags().toString();
+                Document document = new Document("id", id.intValue())
+                        .append("title", title)
+                        .append("link", link)
+                        .append("tag", tag);
+                collection.insertOne(document);
+                id.add(BigInteger.valueOf(1));
+            }
+        });
+        executor.shutdown();
+    }
+    //get max id from mongodb
+    public static int getMaxId(MongoCollection<Document> collection) {
+        int maxId = Integer.MIN_VALUE;
+        FindIterable<Document> findIterable = collection.find();
+        MongoCursor<Document> cursor = findIterable.iterator();
+        int id =0;
+        try {
+            while (cursor.hasNext()) {
+                Document document = cursor.next();
+                 id = Integer.parseInt(document.get("VideoId").toString());
+                if (id>maxId) {
+                    maxId = id;
+                }
+            }
+        } catch (Exception e) {
+            id = 0;
         }
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {
-
+        return maxId;
+    }
+    public static boolean contains(String str) {
+        MongoCollection<Document> collection = database.getCollection("videos");
+        FindIterable<Document> findIterable = collection.find();
+        MongoCursor<Document> cursor = findIterable.iterator();
+        boolean contains = false;
+        while (cursor.hasNext()) {
+            Document document = cursor.next();
+            if (document.get("VideoId").toString().contains(str)) {
+                contains = true;
+                break;
+            }
         }
-            long end = System.currentTimeMillis();
-            System.out.println("耗时：" + (end - start) + "ms");
-            System.out.println("平均耗时：" + (end - start) / 1000.0 / url.size() + "s");
+        return contains;
     }
 }
 
--- src/main/java/org/sexedu/Tools.java
+++ src/main/java/org/sexedu/Tools.java
@@ -48,7 +48,7 @@ public class Tools {
         String str = HttpUtil.createGet(url)
                 .cookie("PHPSESSID=jmjifou28acrclhuil9oj0d5j6; kt_ips=205.198.104.201%2C46.20.109.22")
                 .header("Host", "jable.tv")
-                .setHttpProxy("192.168.6.193", 7890)
+                .setHttpProxy("127.0.0.1", 7890)
                 .execute().body();
         return str;
     }
@@ -73,11 +73,22 @@ public class Tools {
             for (Element element : elements1) {
                 String url1 = element.attr("href");
                 result.add(url1);
+                ++n;
+                if (n>time) {
+                    break;
+                }
             }
-            n+=24;
-            page++;
         }
-        return result;
+        List<String> realResult = new LinkedList();
+        int a = 0;
+        while (true) {
+            realResult.add(result.get(a));
+            a++;
+            if (a>=result.size()-25) {
+                break;
+            }
+        }
+        return realResult;
     }
     public static Result getInfo(Document doc) {
         List<String> tag = new LinkedList<>();
