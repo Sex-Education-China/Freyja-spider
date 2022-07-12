7a2512c87e60911bca3669b133ad8dc487a10354
ppfirework <3212329718@qq.com>
2022-07-12 20:55:32 +0800

    update

--- src/main/java/org/sexedu/Main.java
+++ src/main/java/org/sexedu/Main.java
@@ -17,27 +17,37 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     static MongoClient client = new MongoClient("127.0.0.1", 27017);
     static MongoDatabase database = client.getDatabase("bornhub");
+    static MongoCollection<Document> collection = database.getCollection("videos");
+    static FindIterable<Document> findIterable = collection.find();
+    static MongoCursor<Document> cursor = findIterable.iterator();
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         //get all the collections
         MongoCollection<Document> collection = database.getCollection("videos");
         BigInteger id = BigInteger.valueOf(getMaxId(collection));
-        List<String> url = Tools.exploreURL(20);
+        List<String> url = Tools.exploreURL(1000);
         ExecutorService executor = Executors.newFixedThreadPool(8);
         executor.execute(() -> {
             for (String s : url) {
                 Result result = Tools.getInfo(Jsoup.parse(Tools.getHTML(s)));
                 String title = result.getTitle();
-                String link = result.getVideoLink();
+                String link = result.getVideoLink().replace("\r\n", "");
                 String tag = result.getTags().toString();
-                Document document = new Document("id", id.intValue())
-                        .append("title", title)
-                        .append("link", link)
-                        .append("tag", tag);
-                collection.insertOne(document);
-                id.add(BigInteger.valueOf(1));
+                if (!contains(title)) {
+                    Document document = new Document("id", id.toString())
+                            .append("title", title)
+                            .append("link", link)
+                            .append("tag", tag);
+                    collection.insertOne(document);
+                    id.add(BigInteger.valueOf(1));
+                }
             }
         });
         executor.shutdown();
+        while (!executor.isTerminated()) {
+
+        }
+        System.out.println("耗时：: " + (System.currentTimeMillis() - start)/1000 + "秒");
     }
     //get max id from mongodb
     public static int getMaxId(MongoCollection<Document> collection) {
@@ -54,18 +64,15 @@ public class Main {
                 }
             }
         } catch (Exception e) {
-            id = 0;
+            maxId = 0;
         }
         return maxId;
     }
     public static boolean contains(String str) {
-        MongoCollection<Document> collection = database.getCollection("videos");
-        FindIterable<Document> findIterable = collection.find();
-        MongoCursor<Document> cursor = findIterable.iterator();
         boolean contains = false;
         while (cursor.hasNext()) {
             Document document = cursor.next();
-            if (document.get("VideoId").toString().contains(str)) {
+            if (document.toString().contains(str)) {
                 contains = true;
                 break;
             }
